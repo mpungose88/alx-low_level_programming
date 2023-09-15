3bf8690f26495f2849372be43f3d2121597efc92
mpungose88 <mpungosesibusiso88@gmail.com>
2023-09-14 19:49:41 -0700

    0x03. C - Debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -17,8 +17,8 @@ int main(void)
 
         /*}*/
 
-        printf("Infinite loop avoided! \\o/\n");
+           printf("Infinite loop avoided! \\o/\n");
 
-        return (0);
+           return (0);
 }
 
