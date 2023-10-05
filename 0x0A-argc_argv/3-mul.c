3b41ec73b2b7d98e633b39d0a4de4edf1e8c9df3
mpungose88 <mpungosesibusiso88@gmail.com>
2023-10-04 20:46:38 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -36,13 +36,11 @@ int _atoi(char *s)
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
+			i++;
 		}
-		i++;
 	}
-
 	if (f == 0)
 		return (0);
-
 	return (n);
 }
 
@@ -53,6 +51,7 @@ int _atoi(char *s)
  *
  * Return: 0 (Success), 1 (Error)
  */
+
 int main(int argc, char *argv[])
 {
 	int result, num1, num2;
@@ -62,12 +61,9 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
-
 	printf("%d\n", result);
-
 	return (0);
 }
