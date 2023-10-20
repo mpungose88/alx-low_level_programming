f0906c312f4ef3877318832535ca6dde874945de
mpungose88 <mpungosesibusiso88@gmail.com>
2023-10-20 15:37:27 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,22 +7,18 @@
  *
  * Return: the number of nodes printed
  */
-size_t print_list(const list_t *h);
+size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		if (!h->str)
-		{
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
-		}
 		else
-		{
-		printf("[%u] %s\n", h->len, h->str);
-		}
+			printf("[%d] %s\n", h->len, h->str);
+		count++;
 		h = h->next;
-		s++;
 	}
-	return (s);
+	return (count);
 }
