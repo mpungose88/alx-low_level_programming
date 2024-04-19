d09c4220c6d5cb985f2f77ca9a4aa7402feca2de
mpungose88 <mpungosesibusiso88@gmail.com>
2024-04-19 05:00:00 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-
 /**
- * main - prints the name of the file
- *
- * Return: Always 0 (Success)
- */
-int ain(void)
+*main - prints the name of the file
+*
+*Return: always 0
+*/
+
+
+int main(void)
 {
-	printf("%s\n", __FILE__);
-	return (0);
+   printf("%s\n", __FILE__);
+   return (0);
 }
